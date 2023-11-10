2ef442ac2fedbc60b2e64863eb0c01fbf2bddd85
jojoss <beigecanon@gmail.com>
2023-11-10 10:42:37 -0800

    check guess

--- Meiqing_Shi_wordle/src/game/GameContext.jsx
+++ Meiqing_Shi_wordle/src/game/GameContext.jsx
@@ -31,7 +31,7 @@ export const GameProvider = ({ children }) => {
   const [guesses, setGuess] = useState([]);
   const [validWords, setValidWords] = useState([]);
   const [answerWord, setAnswerWord] = useState('');
-
+  const [guessesWithClues, setGuessesWithClues] = useState([]);
 
   const changeDifficulty = (newDifficulty) => {
     setDifficulty(newDifficulty);
@@ -52,6 +52,18 @@ export const GameProvider = ({ children }) => {
     }
   }, [validWords]);
 
+  useEffect(() => {
+    // 一旦guesses更新，更新带有线索的猜测
+    const newGuessesWithClues = guesses.map((guess) => {
+      return guess === answerWord
+        ? { guess, clues: Array(guess.length).fill({ letter: guess, color: 'green' }) }
+        : { guess, clues: checkGuess(guess, answerWord) };
+    });
+    setGuessesWithClues(newGuessesWithClues);
+  }, [guesses, answerWord]);
+
+  
+
   const addLetter = (letter) => {
     if (currentGuess.length < maxGuessLength) {
       setCurrentGuess(currentGuess + letter);
@@ -65,7 +77,6 @@ export const GameProvider = ({ children }) => {
   };
 
   const submitGuess = () => {
-    // TODO: check validity
     if (currentGuess.length !== maxGuessLength) {
       console.log('Word length is invalid.');
       return;
@@ -76,6 +87,14 @@ export const GameProvider = ({ children }) => {
       console.log('Word is invalid.');
       return;
     }  
+
+    if (currentGuess.toLowerCase() === answerWord) {
+      console.log('Congratulations!');
+    } else {
+      const clues = checkGuess(currentGuess, answerWord);
+      console.log(clues);
+    }
+  
   
     setGuess([...guesses, currentGuess]);
     setCurrentGuess('');
@@ -83,6 +102,36 @@ export const GameProvider = ({ children }) => {
     console.log("Enter was pressed, currentGuess:", currentGuess);
     console.log("The answer word is:", answerWord);
   };
+
+
+  const checkGuess = (guess, answer) => {
+    const result = [];
+  
+    // 使用一个对象来跟踪答案单词中的字母及其数量
+    const letterCount = {};
+  
+    // 首先记录答案中每个字母的数量
+    for (let letter of answer) {
+      letterCount[letter] = (letterCount[letter] || 0) + 1;
+    }
+  
+    // 检查每个字母是否在正确的位置
+    for (let i = 0; i < guess.length; i++) {
+      const letter = guess[i];
+      if (answer[i] === letter) {
+        result.push({ letter, color: 'green' });
+        letterCount[letter]--;
+      } else if (answer.includes(letter) && letterCount[letter] > 0) {
+        result.push({ letter, color: 'yellow' });
+        letterCount[letter]--;
+      } else {
+        result.push({ letter, color: 'grey' });
+      }
+    }
+  
+    return result;
+  };
+  
 
   return (
     <GameContext.Provider value={{ 
@@ -92,7 +141,9 @@ export const GameProvider = ({ children }) => {
       guesses,
       addLetter,
       delLetter,
-      submitGuess
+      submitGuess,
+      guessesWithClues,
+      checkGuess
     }}>
       {children}
     </GameContext.Provider>
